cc0ec1610facc6505f14fa002b990275c1a35c76
sriharish-oasys <sriharish@oasyscybernetics.in>
2024-11-11 17:48:21 +0530

    Attempting New Medium Problem

---
+++ 238/main.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        print(nums)
+        return [1, 2, 3, 4]
+
+
+solution = Solution()
+result = solution.productExceptSelf([1, 2, 3, 4])
